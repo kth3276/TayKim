d7a72b0c01af3c312ca8edcd6892283afb820f97
Your Name <you@example.com>
2018-04-08 01:25:36 +0900

    layout 수정

--- TayKim/settings/common.py
+++ TayKim/settings/common.py
@@ -16,6 +16,7 @@ from os.path import abspath, dirname, join
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = dirname(dirname(dirname(abspath(__file__))))
 
+ROOT = lambda *args: join(BASE_DIR, *args)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -54,6 +55,7 @@ INSTALLED_APPS = [
     'accounts',
     'myblog',
     'shop',
+    'cover',
 ]
 
 MIDDLEWARE = [
--- TayKim/urls.py
+++ TayKim/urls.py
@@ -6,13 +6,14 @@ from django.shortcuts import redirect, render
 
 urlpatterns = [
 
-    url(r'^$', lambda request: render(request, 'root.html'), name='root'),
+    url(r'^$', lambda request: render(request, 'index.html'), name='root'),
     # url(r'^$', lambda r: redirect('myblog:post_list'), name='root'),
     url(r'^admin/', admin.site.urls),
     url(r'^myblog/', include('myblog.urls', namespace='myblog')),
     url(r'^accounts/', include('accounts.urls')),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^shop/', include('shop.urls', namespace='shop')),
+    url(r'^cover/', include('cover.urls', namespace='cover')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
